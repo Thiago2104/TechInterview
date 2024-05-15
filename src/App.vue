7ddc4955f1c6519a6e3595e53b1e5b01a23cda6f
Thiago2104 <santijipin@gmail.com>
2024-05-15 11:43:50 -0500

    Style nav and app-bar
    Wrap NavButton as component, adjust alignment of nav inside app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,44 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <div
+        class="d-flex align-center justify-space-between"
+        style="width: 100%"
+      >
+        <div class="d-flex">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+          />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-        <v-container>
-          <v-row>
-            <v-col>
-              <router-link :to="{ name: 'Home' }">
-                <v-btn variant="flat">Home</v-btn>
-              </router-link>
-            </v-col>
-            <v-col>
-              <router-link :to="{ name: 'About' }">
-                <v-btn variant="flat">About</v-btn>
-              </router-link>
-            </v-col>
-            <v-col>
-              <router-link :to="{ name: 'Data' }">
-                <v-btn variant="flat">Data</v-btn>
-              </router-link>
-            </v-col>
-          </v-row>
-        </v-container>
+          <v-img
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+            width="100"
+          />
+        </div>
+        <div class="d-flex">
+          <v-container>
+            <v-row>
+              <v-col>
+                <NavButton pageName="Home" label="Home" />
+              </v-col>
+              <v-col>
+                <NavButton pageName="Data" label="Data" />
+              </v-col>
+              <v-col>
+                <NavButton pageName="About" label="About" />
+              </v-col>
+            </v-row>
+          </v-container>
+        </div>
       </div>
 
       <v-spacer></v-spacer>
@@ -50,9 +51,13 @@
 </template>
 
 <script>
+import NavButton from "./components/NavButton.vue";
 export default {
   name: "App",
 
+  components: {
+    NavButton,
+  },
   data: () => ({
     //
   }),
---
+++ src/components/NavButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-link :to="{ name: pageName }">
+    <v-btn color="white" text>{{ label }}</v-btn>
+  </router-link>
+</template>
+<script>
+export default {
+  props: {
+    pageName: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
