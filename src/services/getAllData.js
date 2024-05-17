8b953a3b7a9dedea72a11e1e9fe69a4c4759d8ca
Thiago2104 <santijipin@gmail.com>
2024-05-17 10:02:08 -0500

    Modify APIURL
    Now size and type of data are customizable

--- src/services/getAllData.js
+++ src/services/getAllData.js
@@ -1,4 +1,15 @@
-const APIURL = "https://random-data-api.com/api/v2/users?size=90";
+// Set the type of data to fetch (e.g., users, addresses, etc.)
+const type = "users";
+
+const APIURL = new URL(`https://random-data-api.com/api/v2/${type}`);
+
+let params = new URLSearchParams();
+
+// Set the number of items to fetch
+params.set("size", 10);
+params.set("response_type", "json");
+
+APIURL.search = params;
 
 export const load = async () => {
   try {
@@ -12,7 +23,7 @@ export const load = async () => {
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
-    return await response.json();
+    return response.json();
   } catch (err) {
     console.error(err);
     throw new Error("Error fetching data");
