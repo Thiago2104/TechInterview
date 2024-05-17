a626fe7bd2137899b2af516be7f775f26d084d5f
Thiago2104 <santijipin@gmail.com>
2024-05-16 23:59:35 -0500

    Add search function to DataTable

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -13,7 +13,19 @@
             when complete.
           </p></v-card-subtitle
         >
-        <v-data-table v-if="data.length > 0" :headers="headers" :items="data">
+        <v-data-table
+          v-if="data.length > 0"
+          :headers="headers"
+          :items="data"
+          :search="search"
+        >
+          <template v-slot:top>
+            <v-text-field
+              v-model="search"
+              label="Search"
+              class="mx-4"
+            ></v-text-field>
+          </template>
         </v-data-table>
         <v-progress-linear v-else indeterminate></v-progress-linear>
       </v-card>
@@ -25,11 +37,22 @@
 <script>
 import { mapState } from "vuex";
 export default {
+  data() {
+    return {
+      search: "",
+    };
+  },
   computed: {
     ...mapState(["users", "loading"]),
     data() {
       return this.$store.state.users;
     },
+    /* Note: This could expose sensitive data or data out of interest; consider filtering or formatting the data on the backend, and if not possible, make a list of the desired headers to be shown. For example:
+    headers: [
+          { text: "ID", value: "ID" },
+          { text: "First Name", value: "first_name" },
+          { text: "Last Name", value: "last_name" },
+        ],*/
     headers() {
       return Object.keys(this.data[0]).map((key) => {
         const formattedKey = key
@@ -43,31 +66,5 @@ export default {
   mounted() {
     this.$store.dispatch("fetchUsers");
   },
-  /*mounted() {
-    const { load } = getAllData();
-    load()
-      .then((data) => {
-        this.data = data.users;
-        // Get all keys from the data, and format them as column names by removing '_' and capitalizing each word
-        this.headers = Object.keys(this.data[0]).map((key) => {
-          const formattedKey = key
-            .split("_")
-            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(" ");
-          return { text: formattedKey, value: key };
-        });
-        console.log(this.data);
-        Note: This could expose sensitive data or data out of interest; consider filtering or formatting the data on the backend, and if not possible, make a list of the desired headers to be shown. For example:
-        headers: [
-          { text: "ID", value: "ID" },
-          { text: "First Name", value: "first_name" },
-          { text: "Last Name", value: "last_name" },
-        ],
-        
-      })
-      .catch((err) => {
-        this.error = err;
-      });
-  },*/
 };
 </script>
